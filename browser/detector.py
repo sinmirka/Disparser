1bd9cd5203cfe31100890d5a871ffdfccdb84e9d
sinmirka <oskar.rayapu@mail.ru>
2026-06-20 22:07:24 +0500

    feat: add default browser detection

--- browser/detector.py
+++ browser/detector.py
@@ -1,7 +1,12 @@
 from winreg import HKEY_CURRENT_USER, OpenKey, QueryValueEx
 
-def get_default_browser():
+def get_default_browser() -> str:
     key_path = (
         r"Software\Microsoft\Windows\Shell\Associations"
         r"\UrlAssociations\http\UserChoice"
     )
+
+    with OpenKey(HKEY_CURRENT_USER, key_path) as key:
+        prog_id, _ = QueryValueEx(key, "ProgId")
+
+    return prog_id
